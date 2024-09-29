bdfab10d3bef1ef3b906d4cfcae0eb50f44369b0
MonkaS <danchik646@gmail.com>
2024-09-29 20:16:57 +0300

    босс (нужно поправить летучих мышей и сделать чтобы босс становился активным когда игрок подходит к арене

---
+++ Assets/Scripts/characters/DarkFairyBoss/DealDamage.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamage : MonoBehaviour
+{
+    public float damage;
+    public float startDamageTime;
+    private float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (time >= startDamageTime)
+        {
+            Health objectHealth = other.GetComponent<Health>();
+
+            if (objectHealth != null && other.CompareTag("Player"))
+            {
+                objectHealth.takeDamage(damage);
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (time >= startDamageTime)
+        {
+            Health objectHealth = other.GetComponent<Health>();
+
+            if (objectHealth != null && other.CompareTag("Player"))
+            {
+                objectHealth.takeDamage(damage);
+            }
+        }
+    }
+}
